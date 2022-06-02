7e0401afa1ea1ffc1238463d5d76eae8b2e6a531
Rajashekarreddy24 <101244107+Rajashekarreddy24@users.noreply.github.com>
2022-06-02 12:03:18 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,19 @@
+# print("Hello World")
+# x=10
+# y =10
+# print(x+y)
+# print("hello")
+# fruits = ["apple","banana","orange"]
+# x,y,z= fruits
+# print(x)
+# print(y)
+# print(z)
+# x=1
+# a= float(x)
+# print(a)
+# x=1.234
+# a=int(x)
+# print(a)
+x=1j
+a=int(x)
+print(a)
